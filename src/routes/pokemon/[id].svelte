f1023973dd1c80c82add3987861aeeabbc16c490
george <luta.george.gabriel@gmail.com>
2021-07-07 13:42:25 +0200

    Added new features.

---
+++ src/routes/pokemon/[id].svelte
@@ -0,0 +1,25 @@
+<script context="module">
+    export async function load({page}) {
+        const id = page.params.id;
+        const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+        const response = await fetch(url);
+        const pokeman = await response.json();
+        return {
+            props: {
+                pokeman
+            }
+        }
+    }
+</script>
+
+<script>
+    export let pokeman
+    console.log(pokeman)
+</script>
+
+<div class="wrapper">
+    <h1 class="text-4xl text-center my-8 uppercase">{pokeman.name}</h1>
+    <div class="img-wrapper flex justify-center">
+        <img class=" h-40 w-40" src={pokeman.sprites['front_default']}>
+    </div>
+</div>
